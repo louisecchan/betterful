87d59e15e136c2adecb55a6726787aeb32967b3d
Louise <louise.cch@gmail.com>
2023-08-07 19:00:44 +0200

    Fix cart broken image link

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -53,7 +53,7 @@ const Cart = () => {
 
           {products?.map((item) => (
             <div className="item" key={item.id}>
-              <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+              <img src={item.img} alt="" />
 
               <div className="details">
                 <h1>{item.title}</h1>
