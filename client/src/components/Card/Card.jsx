db8e8166f057a2d2c6c7a446934837d51c46a3ef
Louise <louise.cch@gmail.com>
2023-08-07 14:24:25 +0200

    sor ju

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ item }) => {
           {item?.attributes.isNew && <span>NEW IN</span>}
           <img
             src={
-              process.env.REACT_APP_UPLOAD_URL +
+              // process.env.REACT_APP_UPLOAD_URL +
               item.attributes?.img?.data?.attributes?.url
             }
             alt=""
@@ -18,7 +18,7 @@ const Card = ({ item }) => {
           />
           <img
             src={
-              process.env.REACT_APP_UPLOAD_URL +
+              // process.env.REACT_APP_UPLOAD_URL +
               item.attributes?.img2?.data?.attributes?.url
             }
             alt=""
